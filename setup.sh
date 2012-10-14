78d66fc18d3a909989a7498d0082b3767f641abf
slyd0g <unxbm3r@gmail.com>
2012-10-14 18:23:18 +0900

    still testing sshkey - cursed as a perpetual n00b

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
-#
+
 # setup my local env
 # vim syntax highlighting, .zshrc config
+# TODO: add other customizations
 
 /bin/mkdir -v ~/.vim
 /bin/cp -arv ftdetect ftplugin indent syntax ~/.vim/
 /bin/cp -v vimrc ~/.vimrc
 /bin/cp -v zshrc ~/.zshrc && source ~/.zshrc
+# Sun Oct 14 18:23:00 JST 2012
